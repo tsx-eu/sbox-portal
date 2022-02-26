99f1c4db33dd910db3aa7f671c121b55d7399c30
Steve ZARETTI <kossolax@gmail.com>
2022-02-26 15:11:57 +0100

    added velocity rotation

--- code/PortalTraveller.cs
+++ code/PortalTraveller.cs
@@ -6,12 +6,19 @@ namespace Sandbox
 		public Vector3 previousOffsetFromPortal { get; set; }
 
 		public void Teleport(Vector3 pos, Rotation rot) {
+			Rotation deltaRotation;
 			Entity.Position = pos;
 
-			if ( Entity is PortalPlayer player )
+			if ( Entity is PortalPlayer player ) {
+				deltaRotation = player.EyeRotation.Inverse * rot;
 				player.SetRotation( rot );
-			else
+			}
+			else {
+				deltaRotation = Entity.Rotation.Inverse * rot;
 				Entity.Rotation = rot;
+			}
+
+			Entity.Velocity *= deltaRotation;
 		}
 
 		public void EnterPortalThreshold(Portal portal) {
