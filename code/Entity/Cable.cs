733325a00386e171f53bd9fe33c80c2179316972
Steve ZARETTI <kossolax@gmail.com>
2022-04-07 18:04:28 +0200

    added corner

--- code/Entity/Cable.cs
+++ code/Entity/Cable.cs
@@ -12,26 +12,112 @@ namespace Sandbox.Entity
 	[Hammer.Path( "portal_cable" )]
 	public partial class Cable : GenericPathEntity
 	{
+		private static IDictionary<string, List<Material>> Materials = new Dictionary<string, List<Material>>();
+		private Material Material {
+			get {
+				return Rand.FromList( Materials[Color + "_" + Type] );
+			}
+		}
+		private Material MaterialEnd {
+			get {
+				return Rand.FromList( Materials[Color + "_" + Type + "_" + Ending] );
+			}
+		}
+		private Material MaterialCorner {
+			get
+			{
+				return Rand.FromList( Materials[Color + "_" + Type + "_" + CableEnding.Corner] );
+			}
+		}
+
+		private static float CableSize = 16f;
+
+		private List<ModelEntity> childs = new List<ModelEntity>();
+
 		[Net] private string NetworkedPathNodesJSON { get; set; }
 
-		private static IDictionary<string, List<Material>> Materials = new Dictionary<string, List<Material>>();
-		private IList<ModelEntity> childs = new List<ModelEntity>();
-
-		public enum CableDirection
-		{
-			Unkown,
-			FloorX,
-			FloorY,
-			WallX,
-			WallY,
-			CeilingX,
-			CeilingY,
+		public enum CableColor {
+			Blue,
+			Orange
+		}
+		public enum CableType {
+			New,
+			Old
+		}
+		public enum CableEnding {
+			OnOff,
+			Timer,
+			Corner
 		};
 
-		public CableDirection Direction { get; set; } = CableDirection.Unkown;
+		[Property( "color")]
+		public CableColor Color { get; set; } = CableColor.Blue;
+		[Property( "type" )]
+		public CableType Type { get; set; } = CableType.New;
+		[Property( "ending" )]
+		public CableEnding Ending { get; set; } = CableEnding.OnOff;
+
+		private static void Init_Textures() {
+			string key = "";
+
+			key = CableColor.Blue + "_" + CableType.New;
+			if ( Materials.ContainsKey( key ) == false ) {
+					Materials[key] = new List<Material> {
+					Material.Load( "materials/signage/indicator_blue_1.vmat" ),
+					Material.Load( "materials/signage/indicator_blue_2.vmat" ),
+					Material.Load( "materials/signage/indicator_blue_3.vmat" ),
+					Material.Load( "materials/signage/indicator_blue_4.vmat" )
+				};
+			}
+
+			key = CableColor.Blue + "_" + CableType.New + "_" + CableEnding.OnOff;
+			if ( Materials.ContainsKey( key ) == false ) {
+					Materials[key] = new List<Material> {
+					Material.Load( "materials/portal_indicators/sign/indicator_unchecked.vmat" )
+				};
+			}
+
+			key = CableColor.Blue + "_" + CableType.New + "_" + CableEnding.Corner;
+			if ( Materials.ContainsKey( key ) == false )
+			{
+				Materials[key] = new List<Material> {
+					Material.Load( "materials/signage/corner_blue.vmat" )
+				};
+			}
+
+
+			key = CableColor.Orange + "_" + CableType.New;
+			if ( Materials.ContainsKey( key ) == false ) {
+				Materials[key] = new List<Material> {
+					Material.Load( "materials/signage/indicator_orange_1.vmat" ),
+					Material.Load( "materials/signage/indicator_orange_2.vmat" ),
+					Material.Load( "materials/signage/indicator_orange_3.vmat" ),
+					Material.Load( "materials/signage/indicator_orange_4.vmat" )
+				};
+			}
+
+			key = CableColor.Orange + "_" + CableType.New + "_" + CableEnding.OnOff;
+			if ( Materials.ContainsKey( key ) == false ) {
+				Materials[key] = new List<Material> {
+					Material.Load( "materials/portal_indicators/sign/indicator_checked.vmat" )
+				};
+			}
+
+			key = CableColor.Orange + "_" + CableType.New + "_" + CableEnding.Corner;
+			if ( Materials.ContainsKey( key ) == false ) {
+				Materials[key] = new List<Material> {
+					Material.Load( "materials/signage/corner_orange.vmat" )
+				};
+			}
+
+
+
+		}
 
 		public Cable() {
 			Transmit = TransmitType.Always;
+
+			Init_Textures();
 		}
 
 		public override void Spawn() {
@@ -39,50 +125,30 @@ namespace Sandbox.Entity
 			NetworkedPathNodesJSON = pathNodesJSON;
 		}
 
-		private static void Initialize_Textures() {
-			if ( Materials.ContainsKey( "blue" ) == false ) {
-				var blues = new List<Material> {
-					Material.Load( "materials/signage/indicator_blue_1.vmat" ),
-					Material.Load( "materials/signage/indicator_blue_2.vmat" ),
-					Material.Load( "materials/signage/indicator_blue_3.vmat" ),
-					Material.Load( "materials/signage/indicator_blue_4.vmat" )
-				};
-
-				Materials["blue"] = blues;
-			}
-
-			if ( Materials.ContainsKey( "orange" ) == false ) {
-				var blues = new List<Material> {
-					Material.Load( "materials/signage/indicator_orange_1.vmat" ),
-					Material.Load( "materials/signage/indicator_orange_2.vmat" ),
-					Material.Load( "materials/signage/indicator_orange_3.vmat" ),
-					Material.Load( "materials/signage/indicator_orange_4.vmat" )
-				};
-
-				Materials["orange"] = blues;
-			}
-		}
-
+		
 		public override void ClientSpawn() {
 			base.ClientSpawn();
+			pathNodesJSON = NetworkedPathNodesJSON;
+			PathNodes = JsonSerializer.Deserialize<List<BasePathNode>>( pathNodesJSON, new JsonSerializerOptions { PropertyNameCaseInsensitive = true } );
 
-			Initialize_Textures();
-			childs.ToList().ForEach( i => i.Delete() );
-			rebuild();
+			childs.ForEach( i => i.Delete() );
+			childs.Clear();
+			Rebuild();
 		}
 
-		private async void rebuild() {
+		private async void Rebuild() {
 			// delay until all entities are spawned.
 			await Task.Delay( 100 );
 
-			var nodes = JsonSerializer.Deserialize<List<BasePathNode>>( NetworkedPathNodesJSON, new JsonSerializerOptions { PropertyNameCaseInsensitive = true } );
 
-			CableDirection dir = Direction;
+			for ( int i = 0; i < PathNodes.Count - 1; i++ ) {
 
-			for ( int i = 0; i < nodes.Count - 1; i++ ) {
+				bool start = (i == 0);
+				bool corner = !PathNodes[i].TangentIn.z.AlmostEqual( PathNodes[i].TangentOut.z );
+				bool last = (i == PathNodes.Count - 2);
 
 				var model = Model.Builder
-					.AddMeshes( CreateMeshesh( nodes[i], nodes[i + 1], Materials["blue"], ref dir, i == 0, i == nodes.Count - 2 ) )
+					.AddMeshes( CreateMeshesh( PathNodes[i], PathNodes[i + 1], start, corner, last ) )
 					.Create();
 
 				var prop = new ModelEntity {
@@ -128,76 +194,82 @@ namespace Sandbox.Entity
 			return Vector3.Up;
 		}
 
-		private Mesh[] CreateMeshesh(BasePathNode src, BasePathNode dst, List<Material> mats, ref CableDirection dir, bool first, bool last)
+		private Mesh[] CreateMeshesh(BasePathNode src, BasePathNode dst, bool first, bool corner, bool last)
 		{
-			void CreateLine( Vector3 position, Rotation rot, Vector3 size, ref bool skipedFirst, ref bool skippedCorner, ref List<Mesh> meshes ) {
+			void CreateQuad( Vector3 position, Rotation rot, Material mat, ref bool skipedFirst, ref bool skippedCorner, ref List<Mesh> meshes ) {
+				if ( !skippedCorner )
+					skippedCorner = true;
+
 				if ( !skipedFirst ) {
 					skipedFirst = true;
-					skippedCorner = true;
-					return;
-				}
-				if ( !skippedCorner ) {
-					skippedCorner = true;
-					return;
+					if( first || corner )
+						return;
 				}
 
 
 				var vb = new VertexBuffer();
 				vb.Init( true );
 
+				Vector3 size = new Vector3( CableSize );
 
 				var f = rot.Forward * 0.1f;
+				var u = rot.Left * size.y * 0.5f;
+				var v = rot.Up * size.x * 0.5f;
 
-				var u = rot.Up * size.x * 0.5f;
-				var v = rot.Left * size.y * 0.5f;
+				vb.AddQuad( new Ray( position + f, f.Normal ), u, v );
+				vb.AddQuad( new Ray( position - f, f.Normal ), u, -v );
 
-				vb.AddQuad( new Ray( position + f, f.Normal ), v, u );
-				vb.AddQuad( new Ray( position - f, f.Normal ), u, v );
+				//
 
-				var mesh = new Mesh( Rand.FromList( mats ) );
+				var mesh = new Mesh( mat );
 				mesh.CreateBuffers( vb );
 				meshes.Add( mesh );
 			}
 
-			int size = 16;
-
 			Vector3 start = src.Position;
 			Vector3 end = dst.Position;
-
 			Rotation rot = GetMeshDirection( src, dst ).EulerAngles.ToRotation();
 
 			List<Mesh> meshes = new List<Mesh>();
 			Vector3 delta = end - start;
 
-			float incrx = Math.Sign( delta.x ) * size;
-			float incry = Math.Sign( delta.y ) * size;
-			float incrz = Math.Sign( delta.z ) * size;
+			float incrx = Math.Sign( delta.x ) * CableSize;
+			float incry = Math.Sign( delta.y ) * CableSize;
+			float incrz = Math.Sign( delta.z ) * CableSize;
 
 			float x = 0, y = 0, z = 0;
 			bool skipedFirst = false, skippedCorner = false;
 
 			skippedCorner = false;
 			for ( x = 0; Math.Abs( x )  < Math.Abs( delta.x ); x += incrx ) {
-				CreateLine(new Vector3( start.x + x, start.y + y, start.z + z), rot, size, ref skipedFirst, ref skippedCorner, ref meshes );
+				CreateQuad( new Vector3( start.x + x, start.y + y, start.z + z), rot, !skippedCorner ? MaterialCorner : Material, 
+					ref skipedFirst, ref skippedCorner, ref meshes );
 			}
 
 			skippedCorner = false;
 			for ( y = 0; Math.Abs( y ) < Math.Abs( delta.y ); y += incry ) {
-				CreateLine( new Vector3( start.x + x, start.y + y, start.z + z), rot, size, ref skipedFirst, ref skippedCorner, ref meshes );
+				CreateQuad( new Vector3( start.x + x, start.y + y, start.z + z), rot, !skippedCorner ? MaterialCorner : Material, 
+					ref skipedFirst, ref skippedCorner, ref meshes );
 			}
 
 			skippedCorner = false;
 			for ( z = 0; Math.Abs( z ) < Math.Abs( delta.z ); z += incrz ) {
-				CreateLine( new Vector3( start.x + x, start.y + y, start.z + z), rot, size, ref skipedFirst, ref skippedCorner, ref meshes );
+				CreateQuad( new Vector3( start.x + x, start.y + y, start.z + z), rot, !skippedCorner ? MaterialCorner : Material, 
+					ref skipedFirst, ref skippedCorner, ref meshes );
 			}
 
+
+			if( last ) {
+				CreateQuad( new Vector3( start.x + x, start.y + y, start.z + z ), rot, MaterialEnd,
+					ref skipedFirst, ref skippedCorner, ref meshes );
+			}
 			return meshes.ToArray();			
 		} 
 
-		[Event.Tick.Server]
-		public void OnTick2()
+		[Event.Tick]
+		public void Tick()
 		{
-			DrawPath( 0, true );
+			//DrawPath( 0, true );
 		}
 
 		[ClientCmd("cmd_respawn")]
