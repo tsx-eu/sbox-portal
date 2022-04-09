8ead1f688e565642045adfd2b0b5c13b6998e37c
Steve ZARETTI <kossolax@gmail.com>
2022-04-09 11:06:24 +0200

    wall design

--- code/Entity/WallEmiter.cs
+++ code/Entity/WallEmiter.cs
@@ -86,11 +86,37 @@ namespace PortalGame
 
 			float length = (end - start).Length;
 
+			var f = Vector3.Forward * length * 0.5f;
+			var l = Vector3.Up * 2 * 0.5f;
+			var u = Vector3.Left * 64 * 0.5f;
+
+			var o = f + Vector3.Forward * 8;
+
 			var vb = new VertexBuffer();
 			vb.Init( true );
-			vb.AddCube( new Vector3( 8 + length/2, 0, 0 ), new Vector3(length, 64, 2), Rotation.Identity );
 
-			var mesh = new Mesh( Material.Load( "materials/effects/test.vmat" ) );
+			vb.Default.Normal = f.Normal;
+			vb.Default.Tangent = new Vector4( l.Normal, 1 );
+
+			vb.Add( o + u - f, new Vector2( 0, 1 ) );
+			vb.Add( o + u + f, new Vector2( length / 64f, 1 ) );
+			vb.Add( o - u + f, new Vector2( length / 64f, 0 ) );
+			vb.Add( o - u - f, new Vector2( 0, 0 ) );
+
+			vb.AddTriangleIndex( 4, 3, 2 );
+			vb.AddTriangleIndex( 2, 1, 4 );
+
+			vb.Add( o + u + f, new Vector2( 0, 1 ) );
+			vb.Add( o + u - f, new Vector2( -length / 64f, 1 ) );
+			vb.Add( o - u - f, new Vector2( -length / 64f, 0 ) );
+			vb.Add( o - u + f, new Vector2( 0, 0 ) );
+
+			vb.AddTriangleIndex( 4, 3, 2 );
+			vb.AddTriangleIndex( 2, 1, 4 );
+
+
+
+			var mesh = new Mesh( Material.Load( "materials/effects/test2.vmat" ) );
 			mesh.CreateBuffers( vb );
 
 			var model = Model.Builder
